cd6bb2c28249c4ade8ca085b3b1f05a2f422e0c7
Gabriel Gomes <gabrielsogom@gmail.com>
2022-01-04 21:11:54 -0300

    V1.1.2 Improving SPA resource API behavior

--- src/main/java/com/gomes/dataregister/api/controller/SpaApiController.java
+++ src/main/java/com/gomes/dataregister/api/controller/SpaApiController.java
@@ -3,11 +3,16 @@ package com.gomes.dataregister.api.controller;
 import com.gomes.dataregister.core.service.PropertiesService;
 import com.gomes.dataregister.core.utils.HttpExecutor;
 import net.minidev.json.JSONObject;
+import org.apache.coyote.Response;
 import org.flywaydb.core.internal.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,10 +31,15 @@ public class SpaApiController {
         return httpExecutor.get(propertiesService.getSpaApiUrl()+"/api/v1/spa/" + page + "/" + resource);
     }
 
-    @GetMapping(value = {"/{page}/{file}/{resource}"}, produces = {"application/javascript","text/stylesheet"})
-    public String renderResourceFile(@PathVariable("page") String page, @PathVariable("file") String file, @PathVariable("resource") String resource) {
+    @GetMapping(value = {"/{page}/{file}/{resource}"})
+    public ResponseEntity<String> renderResourceFile(@PathVariable("page") String page, @PathVariable("file") String file, @PathVariable("resource") String resource) {
         HttpExecutor httpExecutor = new HttpExecutor();
-        return httpExecutor.get(propertiesService.getSpaApiUrl()+"/api/v1/spa/" + page + "/" + file + "/" + resource);
-
+        String contentType;
+        if (file.equals("css")) contentType = "text/css";
+        else if (file.equals("js")) contentType = "text/javascript";
+        else contentType = "text/plain";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type",contentType);
+        return new ResponseEntity<String>(httpExecutor.get(propertiesService.getSpaApiUrl()+"/api/v1/spa/" + page + "/" + file + "/" + resource),headers, HttpStatus.OK);
     }
 }
